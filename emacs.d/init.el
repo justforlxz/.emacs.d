9a67dbde8c638b5b9671c08e5d7504bbe55ec5fd
haruyukilxz <lxz@ilxz.me>
2018-07-07 22:39:16 +0800

    add go mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -2,12 +2,16 @@
 
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 
-;;(require 'init-packages)
+(require 'init-packages)
 (require 'init-utils)
 (require 'init-elpa)
 ;;(require 'init-fonts)
 ;;(require 'init-markdown)
 
+(require 'go-autocomplete)
+(require 'auto-complete-config)
+(ac-config-default)
+(global-auto-complete-mode t)
 (provide 'init)
 
 ;; 显示行号
@@ -18,3 +22,29 @@
 
 ;; 关闭启动帮助画面
 (setq inhibit-splash-screen 1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (company-go auto-compile go-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Golang
+
+(require 'go-mode)(add-hook 'before-save-hook 'gofmt-before-save)
+(add-hook 'go-mode-hook '(lambda ()
+  (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)))
+(add-hook 'go-mode-hook '(lambda ()
+  (local-set-key (kbd "C-c C-g") 'go-goto-imports)))
+(add-hook 'go-mode-hook '(lambda ()
+  (local-set-key (kbd "C-c C-f") 'gofmt)))(add-hook 'before-save-hook 'gofmt-before-save)
+(add-hook 'go-mode-hook '(lambda ()
+  (local-set-key (kbd "C-c C-k") 'godoc)))
+(add-hook 'go-mode-hook 'company-mode)(add-hook 'go-mode-hook (lambda ()  (set (make-local-variable 'company-backends) '(company-go))
+  (company-mode)))
--- emacs.d/lisp/init-packages.el
+++ emacs.d/lisp/init-packages.el
@@ -6,29 +6,4 @@
 
 ;; 注意 elpa.emacs-china.org 是 Emacs China 中文社区在国内搭建的一个 ELPA 镜像
 
- ;; cl - Common Lisp Extension
- (require 'cl)
-
- ;; Add Packages
- (defvar my/packages '(
-		nodejs-repl
-		exec-path-from-shell
-		) "Default packages")
-
- (setq package-selected-packages my/packages)
-
- (defun my/packages-installed-p ()
-     (loop for pkg in my/packages
-	   when (not (package-installed-p pkg)) do (return nil)
-	   finally (return t)))
-
- (unless (my/packages-installed-p)
-     (message "%s" "Refreshing package database...")
-     (package-refresh-contents)
-     (dolist (pkg my/packages)
-       (when (not (package-installed-p pkg))
-	 (package-install pkg))))
-
- ;; Find Executable Path on OS X
- (when (memq window-system '(mac ns))
-   (exec-path-from-shell-initialize))
+(provide 'init-packages)
